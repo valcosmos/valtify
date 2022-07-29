e70c2a9fbed37ceda0d5995ac3e37e3994570857
valcosmos <53572196+valcosmos@users.noreply.github.com>
2022-07-29 02:50:57 +0000

    Deploying to gh-pages from @ valcosmos/val-design-vue@66d7a459e668f21b0e6c648f92d5a0bc0b643934 🚀

---
+++ assets/components_button_index.md.421b1269.lean.js
@@ -0,0 +1,19 @@
+import{_ as g,c as v,a as k,w as p,b as t,d as n,r as d,o as h,V as b}from"./app.996342fe.js";const B={name:"component-doc",components:{"render-demo-0":function(){const{createTextVNode:a,resolveComponent:l,withCtx:e,createVNode:s,openBlock:r,createElementBlock:i}=b,o=a("Default Button"),c=a("Primary Button"),m=a("Primary Large Button");function _(N,C){const u=l("v-button");return r(),i("div",null,[s(u,null,{default:e(()=>[o]),_:1}),s(u,{type:"primary"},{default:e(()=>[c]),_:1}),s(u,{type:"primary",size:"large"},{default:e(()=>[m]),_:1})])}return{render:_,...{}}}()}},w=JSON.parse('{"title":"\u6309\u94AE - Button","description":"","frontmatter":{},"headers":[],"relativePath":"components/button/index.md"}'),y=t("h1",{id:"\u6309\u94AE-button",tabindex:"-1"},[n("\u6309\u94AE - Button "),t("a",{class:"header-anchor",href:"#\u6309\u94AE-button","aria-hidden":"true"},"#")],-1),f=t("div",null,"It is a Button component",-1),x=t("div",{class:"language-vue"},[t("pre",null,[t("code",null,[t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("template")]),t("span",{class:"token punctuation"},">")]),n(`
+
+  `),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("v-button")]),t("span",{class:"token punctuation"},">")]),n("Default Button"),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("v-button")]),t("span",{class:"token punctuation"},">")]),n(`
+
+  `),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("v-button")]),n(),t("span",{class:"token attr-name"},"type"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("primary"),t("span",{class:"token punctuation"},'"')]),t("span",{class:"token punctuation"},">")]),n("Primary Button"),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("v-button")]),t("span",{class:"token punctuation"},">")]),n(`
+
+  `),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("v-button")]),n(),t("span",{class:"token attr-name"},"type"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("primary"),t("span",{class:"token punctuation"},'"')]),n(),t("span",{class:"token attr-name"},"size"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("large"),t("span",{class:"token punctuation"},'"')]),t("span",{class:"token punctuation"},">")]),n("Primary Large Button"),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("v-button")]),t("span",{class:"token punctuation"},">")]),n(`
+
+`),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("template")]),t("span",{class:"token punctuation"},">")]),n(`
+`)])])],-1);function E(a,l,e,s,r,i){const o=d("render-demo-0"),c=d("demo");return h(),v("div",null,[y,k(c,{sourceCode:`<template>
+
+  <v-button>Default Button</v-button>
+
+  <v-button type="primary">Primary Button</v-button>
+
+  <v-button type="primary" size="large">Primary Large Button</v-button>
+
+</template>
+`},{description:p(()=>[f]),highlight:p(()=>[x]),default:p(()=>[k(o)]),_:1})])}var A=g(B,[["render",E]]);export{w as __pageData,A as default};
