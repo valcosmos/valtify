77aaf885193510a8a5e0fc953aa76308a36e8034
valcosmos <valcosmos@users.noreply.github.com>
2022-08-07 15:53:13 +0000

    Deploying to gh-pages from @ valcosmos/valtify@8a38946a845487d7cbf039b6649cfebeeef72d89 🚀

---
+++ assets/components_tree_index.md.7914c91e.lean.js
@@ -0,0 +1,115 @@
+import{_,c as b,a as m,w as e,b as n,d as s,r as y,o as v,V as g}from"./app.5503eaac.js";const f={name:"component-doc",components:{"render-demo-0":function(){const{resolveComponent:o,createVNode:p,openBlock:l,createElementBlock:c}=g;function r(a,u){const k=o("VTree");return l(),c("div",null,[p(k,{data:a.data},null,8,["data"])])}const{ref:t}=g;return{render:r,...{setup(a,{expose:u}){u();const d={data:t([{label:"docs",id:"docs"},{label:"packages",id:"packages",expanded:!0,children:[{label:"plugin-vue",id:"plugin-vue"},{label:"vite",id:"vite",expanded:!0,children:[{label:"src",id:"src"},{label:"README.md",id:"README.md"}]}]},{label:"scripts",id:"scripts",children:[{label:"release.ts",id:"release.ts"},{label:"verifyCommit.ts",id:"verifyCommit.ts"}]},{label:"pnpm-workspace.yaml",id:"pnpm-workspace.yaml"}]),ref:t};return Object.defineProperty(d,"__isScriptSetup",{enumerable:!1,value:!0}),d}}}}()}},C=JSON.parse('{"title":"\u{1F332} \u6811","description":"","frontmatter":{},"headers":[],"relativePath":"components/tree/index.md"}'),h=n("h1",{id:"\u{1F332}-\u6811",tabindex:"-1"},[s("\u{1F332} \u6811 "),n("a",{class:"header-anchor",href:"#\u{1F332}-\u6811","aria-hidden":"true"},"#")],-1),E=n("div",null,"this is a tree",-1),x=n("div",{class:"language-vue"},[n("pre",null,[n("code",null,[s("  "),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),s("template")]),n("span",{class:"token punctuation"},">")]),s(`
+    `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),s("VTree")]),s(),n("span",{class:"token attr-name"},":data"),n("span",{class:"token attr-value"},[n("span",{class:"token punctuation attr-equals"},"="),n("span",{class:"token punctuation"},'"'),s("data"),n("span",{class:"token punctuation"},'"')]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),s("VTree")]),n("span",{class:"token punctuation"},">")]),s(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),s("template")]),n("span",{class:"token punctuation"},">")]),s(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),s("script")]),s(),n("span",{class:"token attr-name"},"setup"),n("span",{class:"token punctuation"},">")]),n("span",{class:"token script"},[n("span",{class:"token language-javascript"},[s(`
+    `),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" ref "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},"'vue'"),s(`
+    
+    `),n("span",{class:"token keyword"},"const"),s(" data "),n("span",{class:"token operator"},"="),s(),n("span",{class:"token function"},"ref"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"["),s(`
+      `),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'docs'"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'docs'"),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'packages'"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'packages'"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"expanded"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"children"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"["),s(`
+          `),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'plugin-vue'"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'plugin-vue'"),s(`
+          `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+          `),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'vite'"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'vite'"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token literal-property property"},"expanded"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token literal-property property"},"children"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"["),s(`
+              `),n("span",{class:"token punctuation"},"{"),s(`
+                `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'src'"),n("span",{class:"token punctuation"},","),s(`
+                `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'src'"),s(`
+              `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+              `),n("span",{class:"token punctuation"},"{"),s(`
+                `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'README.md'"),n("span",{class:"token punctuation"},","),s(`
+                `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'README.md'"),s(`
+              `),n("span",{class:"token punctuation"},"}"),s(`
+            `),n("span",{class:"token punctuation"},"]"),s(`
+          `),n("span",{class:"token punctuation"},"}"),s(`
+        `),n("span",{class:"token punctuation"},"]"),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'scripts'"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'scripts'"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"children"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"["),s(`
+          `),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'release.ts'"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'release.ts'"),s(`
+          `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+          `),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'verifyCommit.ts'"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'verifyCommit.ts'"),s(`
+          `),n("span",{class:"token punctuation"},"}"),s(`
+        `),n("span",{class:"token punctuation"},"]"),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token literal-property property"},"label"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'pnpm-workspace.yaml'"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token literal-property property"},"id"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token string"},"'pnpm-workspace.yaml'"),s(`
+      `),n("span",{class:"token punctuation"},"}"),s(`
+    `),n("span",{class:"token punctuation"},"]"),n("span",{class:"token punctuation"},")"),s(`
+  `)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),s("script")]),n("span",{class:"token punctuation"},">")]),s(`
+`)])])],-1);function V(o,p,l,c,r,t){const i=y("render-demo-0"),a=y("demo");return v(),b("div",null,[h,m(a,{sourceCode:`  <template>
+    <VTree :data="data"></VTree>
+  </template>
+  <script setup>
+    import { ref } from 'vue'
+    
+    const data = ref([
+      {
+        label: 'docs',
+        id: 'docs'
+      },
+      {
+        label: 'packages',
+        id: 'packages',
+        expanded: true,
+        children: [
+          {
+            label: 'plugin-vue',
+            id: 'plugin-vue'
+          },
+          {
+            label: 'vite',
+            id: 'vite',
+            expanded: true,
+            children: [
+              {
+                label: 'src',
+                id: 'src'
+              },
+              {
+                label: 'README.md',
+                id: 'README.md'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'scripts',
+        id: 'scripts',
+        children: [
+          {
+            label: 'release.ts',
+            id: 'release.ts'
+          },
+          {
+            label: 'verifyCommit.ts',
+            id: 'verifyCommit.ts'
+          }
+        ]
+      },
+      {
+        label: 'pnpm-workspace.yaml',
+        id: 'pnpm-workspace.yaml'
+      }
+    ])
+  <\/script>
+`},{description:e(()=>[E]),highlight:e(()=>[x]),default:e(()=>[m(i)]),_:1})])}var B=_(f,[["render",V]]);export{C as __pageData,B as default};
