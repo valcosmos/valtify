19171170130463d4ecc9889c14a5c1536c88420b
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2022-09-21 14:48:38 +0800

    test: optimize skeleton and tree component tests

--- src/skeleton/test/skeleton.test.ts
+++ src/skeleton/test/skeleton.test.ts
@@ -3,7 +3,11 @@ import Skeleton from '../src/skeleton'
 
 describe('skeleton test', () => {
   test('skeleton init render', async () => {
-    const { getByRole } = render(Skeleton)
-    getByRole('skeleton')
+    const { getByText } = render(Skeleton, {
+      props: { bg: true },
+      slots: { default: () => 'skeleton' }
+    })
+    const sk = getByText('skeleton')
+    expect(sk.classList.contains('v-skeleton'))
   })
 })
