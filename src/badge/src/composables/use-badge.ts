b332b3be23d7a4d579beea8170a6b46b2e644e72
Cupid Valentine <westerngod@126.com>
2022-09-21 21:57:22 +0800

    refactor: optimized badge component code

---
+++ src/badge/src/composables/use-badge.ts
@@ -0,0 +1,21 @@
+import { computed } from 'vue'
+import { isString } from '../../../shared/utils'
+import { BadgeProps } from '../badge-type'
+
+export const useBadge = () => {
+  const useShow = (props: BadgeProps) =>
+    computed(() => {
+      if (props.hidden) return false
+      if (props.value === 0) return false
+      return true
+    })
+
+  const useBadgeContent = (props: BadgeProps) =>
+    computed(() => {
+      if (!props.max || isString(props.value)) return props.value
+      if (props.value <= props.max) return props.value
+      return `${props.max}+`
+    })
+
+  return { useShow, useBadgeContent }
+}
