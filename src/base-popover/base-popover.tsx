8f8ea5283fa26d72d1194ce3f82af13665e1ec24
Cupid Valentine <westerngod@126.com>
2022-10-25 23:25:10 +0800

    feat: add arrow in popover

--- src/base-popover/base-popover.tsx
+++ src/base-popover/base-popover.tsx
@@ -1,7 +1,6 @@
-import { defineComponent, PropType, ref, toRefs, watch } from 'vue'
-import { computePosition } from '@floating-ui/dom'
+import { defineComponent, PropType, ref, toRefs, watch, nextTick } from 'vue'
+import { computePosition, arrow, offset } from '@floating-ui/dom'
 
-import { nextTick } from 'process'
 export default defineComponent({
   name: 'VBasePopover',
   props: {
@@ -12,15 +11,19 @@ export default defineComponent({
     host: {
       type: Object as PropType<HTMLElement>,
       default: () => null
+    },
+    showArrow: {
+      type: Boolean,
+      default: false
     }
   },
   emits: ['update:modelValue'],
   setup(props, { slots, attrs }) {
     // 获取属性中的关键值
-    const { host: hostRef, modelValue } = toRefs(props)
+    const { host: hostRef, modelValue, showArrow } = toRefs(props)
 
     //宿主元素
-    // const hostRef = ref()
+    const arrowRef = ref()
 
     // 浮动元素
     const overlayRef = ref()
@@ -28,13 +31,29 @@ export default defineComponent({
     // 计算定位
 
     const updatePosition = () => {
-      computePosition(hostRef.value, overlayRef.value).then(({ x, y }) => {
-        // 设置overlay
-        Object.assign(overlayRef.value.style, {
-          left: x + 'px',
-          top: y + 'px'
-        })
-      })
+      const middleware: any = []
+      if (showArrow.value) {
+        middleware.push(offset(8))
+        middleware.push(arrow({ element: arrowRef.value }))
+      }
+
+      computePosition(hostRef.value, overlayRef.value, { middleware }).then(
+        ({ x, y, middlewareData, placement }) => {
+          // 设置overlay
+          Object.assign(overlayRef.value.style, {
+            left: x + 'px',
+            top: y + 'px'
+          })
+
+          // 每次计算结束，重新计算肩头定位样式
+          Object.assign(arrowRef.value.style, {
+            left: middlewareData.arrow?.x + 'px',
+            top: '-4px',
+            'border-bottom-color': 'transparent',
+            'border-right-color': 'transparent'
+          })
+        }
+      )
     }
 
     watch(modelValue, newValue => {
@@ -48,7 +67,13 @@ export default defineComponent({
       <>
         {modelValue.value && (
           <div ref={overlayRef} class="v-base-popover" {...attrs}>
+            {/* 弹窗内容 */}
             {slots.default?.()}
+
+            {/* 箭头 */}
+            {showArrow.value && (
+              <div class={'v-base-popover__arrow'} ref={arrowRef}></div>
+            )}
           </div>
         )}
       </>
